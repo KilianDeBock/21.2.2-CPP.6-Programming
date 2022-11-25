050cfbc38efcbec09dae5768f8cd63844012382c
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-11-25 14:00:04 +0100

    22.47.5.4: Undo copy

--- Exercises/Tracky/src/app/Components/Design/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import { Image } from "react-native";
-
-const Logo = ({ style }) => {
-  return (
-    <Image style={style} source={require("../../../../../assets/logo.png")} />
-  );
-};
-
-export default Logo;
--- Exercises/Tracky/src/core/utils/padTime.js
+++
@@ -1,5 +0,0 @@
-const padTime = (time) => {
-  return parseInt(time) < 10 ? `0${time}` : time;
-};
-
-export default padTime;
